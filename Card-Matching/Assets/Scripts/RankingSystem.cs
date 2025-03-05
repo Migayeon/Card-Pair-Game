42c00943d106283a513bdfcc8f663e73ec29a3f9
Migayeon <edisonjini@naver.com>
2025-03-05 23:39:27 +0900

    Ranking Done

--- Card-Matching/Assets/Scripts/RankingSystem.cs
+++ Card-Matching/Assets/Scripts/RankingSystem.cs
@@ -12,8 +12,12 @@ public class RankingSystem : MonoBehaviour
     public static string playerName;
     private int pivotPage = 0;
     private int maxPage = 0;
+    Ranking ranking;
 
     public Transform rankingBlocks;
+
+    [SerializeField] GameObject controller;
+    [SerializeField] Text pageText;
 
     [SerializeField]
     struct Ranking
@@ -32,9 +36,28 @@ public class RankingSystem : MonoBehaviour
 
     }
 
+    public void PrevPage()
+    {
+        if (pivotPage > 0 )
+        {
+            pivotPage--;
+            pageText.text = (pivotPage + 1) + " / " + (maxPage + 1);
+            UpdateRankPage(pivotPage);
+        }
+    }
+
+    public void NextPage()
+    {
+        if (pivotPage < maxPage)
+        {
+            pivotPage++;
+            pageText.text = (pivotPage + 1) + " / " + (maxPage + 1);
+            UpdateRankPage(pivotPage);
+        }
+    }
 
 
-    private void UpdateRankPage(int pivot, Ranking ranking)
+    private void UpdateRankPage(int pivot)
     {
         int limit = ranking.records.Count;
         for (int i = 0; i < Mathf.Min(limit - (pivot*10), 10); i++)
@@ -57,7 +80,7 @@ public class RankingSystem : MonoBehaviour
     {
         string path = Path.Combine(Application.persistentDataPath, "Ranking.json");
         Debug.Log(path);
-        Ranking ranking = new Ranking();
+        ranking = new Ranking();
         if(!File.Exists(path))
         {
             ranking.names = new List<string>();
@@ -70,10 +93,10 @@ public class RankingSystem : MonoBehaviour
 
         int limit = ranking.records.Count;
         maxPage = limit / 10;
-        int currentPlayerIndex = 0;
+        
         if (record >= 0f)
         {
-            currentPlayerIndex = limit;
+            int currentPlayerIndex = limit;
             for (int i = 0; i < limit; i++)
             {
                 if(record < ranking.records[i])
@@ -85,33 +108,24 @@ public class RankingSystem : MonoBehaviour
             ranking.records.Insert(currentPlayerIndex, record);
             ranking.names.Insert(currentPlayerIndex, playerName);
             limit++;
-        }
-        /*
-        for (int i = 0; i < Mathf.Min(limit, 10); i++)
-        {
-            int j = i;
-            rankingBlocks.GetChild(j).GetChild(0).GetComponent<Text>().text = (j + 1).ToString();
-            rankingBlocks.GetChild(j).GetChild(1).GetComponent<Text>().text = ranking.names[j];
-            rankingBlocks.GetChild(j).GetChild(2).GetComponent<Text>().text = ranking.records[j].ToString("0.00");
-        }
-        for (int i = limit; i < 10; i++)
-        {
-            int j = i;
-            rankingBlocks.GetChild(j).GetChild(0).GetComponent<Text>().text = "";
-            rankingBlocks.GetChild(j).GetChild(1).GetComponent<Text>().text = "";
-            rankingBlocks.GetChild(j).GetChild(2).GetComponent<Text>().text = "";
-        }
-        */
 
-        UpdateRankPage(1,ranking);
-       
-        if (record >= 0f)
-        {
             rankingBlocks.GetChild(10).GetChild(0).GetComponent<Text>().text = (currentPlayerIndex + 1).ToString();
             rankingBlocks.GetChild(10).GetChild(1).GetComponent<Text>().text = ranking.names[currentPlayerIndex];
             rankingBlocks.GetChild(10).GetChild(2).GetComponent<Text>().text = ranking.records[currentPlayerIndex].ToString("0.00");
             rankingBlocks.GetChild(10).gameObject.SetActive(true);
+            record = -1f;
         }
+
+        else
+        {
+            pageText.text = "1 / " + (maxPage + 1);
+            controller.SetActive(true);
+
+        }
+
+        UpdateRankPage(0);
+       
+      
 
 
         json = JsonUtility.ToJson(ranking, true);
